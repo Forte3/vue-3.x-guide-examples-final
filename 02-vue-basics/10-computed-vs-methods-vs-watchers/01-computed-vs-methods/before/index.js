334dc3fae78c7ada3aae337e386ce4c02d95c082
Forte <lianghaizhao@icloud.com>
2022-11-01 18:30:07 +0800

    modify computed

--- 02-vue-basics/10-computed-vs-methods-vs-watchers/01-computed-vs-methods/before/index.js
+++ 02-vue-basics/10-computed-vs-methods-vs-watchers/01-computed-vs-methods/before/index.js
@@ -1,14 +1,19 @@
 // 示例：计算属性和方法的区别
 const app = Vue.createApp({
-  data() {
-    return {
-      blogPosts: [
-        "Vue 3.0x 入门实战",
-        "Vue 3.x 完全指南",
-        "React 18 新特性介绍",
-        "JavaScript 基础语法概览",
-      ],
-    };
-  },
+   data() {
+      return {
+         blogPosts: [
+            "Vue 3.0x 入门实战",
+            "Vue 3.x 完全指南",
+            "React 18 新特性介绍",
+            "JavaScript 基础语法概览",
+         ],
+      };
+   },
+   computed: {
+      vuePosts() {
+         return this.blogPosts.filter((blog) => blog.includes('Vue'))
+      }
+   }
 });
 app.mount("#app");
