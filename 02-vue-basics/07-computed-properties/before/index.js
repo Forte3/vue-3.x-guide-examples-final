94fcbb4311852d5f20fbaf48ff29a39c3661b9f0
Forte <lianghaizhao@icloud.com>
2022-11-01 16:23:39 +0800

    modify computed

--- 02-vue-basics/07-computed-properties/before/index.js
+++ 02-vue-basics/07-computed-properties/before/index.js
@@ -1,9 +1,14 @@
 // 示例：使用 computed 处理稍复杂的业务逻辑
 const app = Vue.createApp({
-  data() {
-    return {
-      showAnswer: false,
-    };
-  },
+   data() {
+      return {
+         showAnswer: false,
+      };
+   },
+   computed: {
+      label() {
+         return this.showAnswer ? 'Hide Answer' : 'Show Answer'
+      }
+   }
 });
 app.mount("#app");
