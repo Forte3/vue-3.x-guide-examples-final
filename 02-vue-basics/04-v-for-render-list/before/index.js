494f35a06ce818f1d17390648ab8f52b775a780b
Forte <lianghaizhao@icloud.com>
2022-11-01 15:19:11 +0800

    modify v-for input

--- 02-vue-basics/04-v-for-render-list/before/index.js
+++ 02-vue-basics/04-v-for-render-list/before/index.js
@@ -1,9 +1,24 @@
 // 示例：展示列表数据
 const app = Vue.createApp({
-  data() {
-    return {
-      todos: ["把项目做完", "去超市购物", "看10分钟的书"],
-    };
-  },
+   data() {
+      return {
+         //  todos: ["把项目做完", "去超市购物", "看10分钟的书"],
+         todos: [
+            {
+               content: "把项目做完",
+               isComplete: true
+            },
+            {
+               content: "去超市购物",
+               isComplete: false
+            },
+            {
+               content: "看10分钟的书",
+               isComplete: true
+            },
+
+         ]
+      };
+   },
 });
 app.mount("#app");
