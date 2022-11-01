118e8be0e3f124b6429697e212b60558b8ab92d4
Forte <lianghaizhao@icloud.com>
2022-11-01 19:38:39 +0800

    modify input

--- 02-vue-basics/11-form-input-event-handling/before/index.js
+++ 02-vue-basics/11-form-input-event-handling/before/index.js
@@ -1,3 +1,15 @@
 // 示例：表单输入控件事件处理
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  data() {
+    return {
+      username: ""
+    }
+  },
+  methods: {
+    handleInput(e) {
+      console.log(e.target.value)
+      this.username = e.target.value
+    }
+  }
+});
 app.mount("#app");
