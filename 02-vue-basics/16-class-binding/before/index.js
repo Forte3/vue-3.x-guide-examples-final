f83699504cc22950c31373c0dfdc58a27e2942dc
Forte <lianghaizhao@icloud.com>
2022-11-02 11:42:16 +0800

    modify style

--- 02-vue-basics/16-class-binding/before/index.js
+++ 02-vue-basics/16-class-binding/before/index.js
@@ -1,7 +1,11 @@
 // 示例：根据条件添加或删除 class 样式
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      textBlue: 'text-blue',
+      danger: 'danger',
+      isHiddent: false
+    };
   },
 });
 app.mount("#app");
